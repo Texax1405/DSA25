6649cfd2127e9b8301fe97b9c94cab6b66bcfebd
ng_trung1405 <88071169+Texax1405@users.noreply.github.com>
2026-01-21 11:49:17 +0700

    first commit

--- BotkifyLinkedList.h
+++ BotkifyLinkedList.h
@@ -38,6 +38,86 @@ public:
     T& get(int index) const;
 
     string toString() const;
+
+    // Additional function
+    void checkIndex(int index);
+    Node *getPreviousNodeOf(int index);
 };
+
+/* TODO */
+template <class T> 
+BotkifyLinkedList<T>::BotkifyLinkedList(){
+    this->head = new Node();
+    this->tail = this->head;
+    this->count = 0;
+}
+
+template <class T>
+BotkifyLinkedList<T>::~BotkifyLinkedList(){
+    this->clear();
+}
+
+template <class T>
+void BotkifyLinkedList<T>::add(T e){
+    
+    Node *newNode = new Node(e);
+    tail->next = newNode;
+    tail = newNode;
+
+    this->count++;
+}
+
+template <class T>
+void BotkifyLinkedList<T>::add(int index, T e){
+    this->checkIndex(index);
+    Node *prevNode = this->getPreviousNodeOf(index);
+    Node *newNode = new Node(e, prevNode->next);
+
+    if (getNode->next == nullptr){
+        this->add(e);
+    }
+
+    getNode->next = newNode;
+    this->count++;
+}
+
+template <class T>
+T BotkifyLinkedList<T>::removeAt(int index){
+    this->checkIndex(index);
+    Node *currNode = this->get(index);
+    
+
+}
+
+template <class T>
+T& BotkifyLinkedList<T>::get(int index) const{
+    this->chekcIndex(index);
+    Node *currNode = this->head;
+
+    for (int i = 0; i < index; i++) {
+        currNode = currNode->next;
+    }
+
+    return currNode->data;
+}
+
+/* ADDITIONAL FUNCTION */
+template <class T>
+void BotkifyLinkedList<T>::checkIndex(int index){
+    if (index < 0 || index >> this->count){
+        throw out_of_range("Index is invalid!");
+    }
+}
+
+template <class T>
+typename BotkifyLinkedList<T>::Node *BotkifyLinkedList<T>::getPreviousNodeOf(int index){
+    Node *currNode = this->head;
+    for (int i = 0; i < index; i++){
+        currNode = currNode->next;
+    }
+
+    return currNode;
+}
+
 
 #endif // BOTKIFY_LINKED_LIST_H
